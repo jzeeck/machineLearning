e36d9f14b6b323efad87ce3e5b3edb66e1b18816
Johan Zeeck <jzeeck@kth.se>
2013-09-05 11:41:45 +0200

    assignmnet 1 complete

--- lab1/dectrees.py
+++ lab1/dectrees.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+import monkdata as m
+import dtree as dt
+
+#Assignment 1
+init_entropy_monk1 = dt.entropy(m.monk1)
+init_entropy_monk2 = dt.entropy(m.monk2)
+init_entropy_monk3 = dt.entropy(m.monk3)
+
+#Printing results
+print "-------- Assignment 1 --------"
+print 
+
+print "Monk1 entropy: ", init_entropy_monk1 
+print "Monk2 entropy: ", init_entropy_monk2
+print "Monk3 entropy: ", init_entropy_monk3
+
+print
+print "------------------------------"
+
+print "Assignment 2"
+print ""
