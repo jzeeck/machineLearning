27c634e0fb9734f453c1075f7092886990f36d1b
Johan Zeeck <jzeeck@kth.se>
2013-09-09 14:53:57 +0200

    adding the instructions to the lab

---
+++ lab2 - Support Vector Machines/svm.py
@@ -0,0 +1,9 @@
+#from cvxopt.solvers import qp
+#from cvxopt.base import matrix
+import numpy
+#import pylab
+import random
+import math
+
+import numpy.distutils.system_info as sysinfo
+print sysinfo.get_info('atlas')
