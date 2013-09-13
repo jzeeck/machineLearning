05e8030c96b697b0ab00a0e021c13af24e9f9d05
Johan Zeeck <jzeeck@kth.se>
2013-09-13 17:35:45 +0200

    pushing up some images

--- lab2 - Support Vector Machines/svm.py
+++ lab2 - Support Vector Machines/svm.py
@@ -11,44 +11,44 @@ import scipy
 
 
 print "Defining functions"
-def RBFKernel(datapoint1, datapoint2, sigma=1):
+def RBFKernel(datapoint1, datapoint2, sigma=1, dummy=0):
 	"Radial Basis Function kernels"
 	value = []
 	value.append(datapoint1[0]-datapoint2[0])
 	value.append(datapoint1[1]-datapoint2[1])
 	return math.exp(-(np.dot(value,value)/2*sigma**2))
 
-def polyKernels(datapoint1, datapoint2, power=5):
+def polyKernels(datapoint1, datapoint2, power=5, dummy=0):
 	"Polynomial kernel function"
 	return math.pow(np.dot(datapoint1, datapoint2) + 1, power)
 
-def linKernel(datapoint1, datapoint2):
+def linKernel(datapoint1, datapoint2, dummya=0, dummyb=0):
 	"Polynomial kernel function"
 	return np.dot(datapoint1, datapoint2) + 1
 
-def sigmoidKernel(datapoint1, datapoint2, k=0.05, delta=-10):
+def sigmoidKernel(datapoint1, datapoint2, k=0.05, delta=10):
 	"Sigmoid kernel function"
-	return math.tanh(k*np.dot(datapoint1, datapoint2) - delta)
+	return math.tanh(k*np.dot(datapoint1, datapoint2) + delta)
 
-def buildPMatrix(datapoints, kernel):
+def buildPMatrix(datapoints, kernel, par1, par2):
 	tempMatrix = matrix(0.1,(len(datapoints),len(datapoints)))
 	for i in range(len(datapoints)):
 		for j in range(len(datapoints)):
 			#print "solving P, point (x, y) (",i,", ",j,")"
 			tempMatrix[i,j] = datapoints[i][2]*datapoints[j][2]*kernel(
-				[datapoints[i][0], datapoints[i][1]], [datapoints[j][0], datapoints[j][1]])
+				[datapoints[i][0], datapoints[i][1]], [datapoints[j][0], datapoints[j][1]], par1, par2)
 	return tempMatrix
 
-def indicator(xPoint, yPoint, alphaPoints, kernel):
+def indicator(xPoint, yPoint, alphaPoints, kernel, par1, par2):
 	indValue = 0
 	for alphaPoint in alphaPoints:
-		indValue += alphaPoint[0]*alphaPoint[1][2]*kernel([xPoint, yPoint],[alphaPoint[1][0], alphaPoint[1][1]])
+		indValue += alphaPoint[0]*alphaPoint[1][2]*kernel([xPoint, yPoint],[alphaPoint[1][0], alphaPoint[1][1]], par1, par2)
 	return indValue
 
-def svmMain(indata, kernel, figure):
+def svmMain(indata, kernel, figure, par1, par2):
 	print "Defining the variables needed for vecktor machine"
 	N = len(indata)
-	P = matrix(buildPMatrix(indata, kernel))
+	P = matrix(buildPMatrix(indata, kernel, par1, par2))
 	G = spmatrix(-1.0, range(N), range(N))
 	q = matrix(np.linspace(-1,-1,N))
 	h = matrix(np.zeros(N))
@@ -80,7 +80,7 @@ def svmMain(indata, kernel, figure):
 	xrange = np.arange(-4, 4, 0.05)
 	yrange = np.arange(-4, 4, 0.05)
 
-	grid = matrix([[indicator(x, y, indicationMapping, kernel)
+	grid = matrix([[indicator(x, y, indicationMapping, kernel, par1, par2)
 		for y in yrange]
 		for x in xrange])
 
@@ -108,13 +108,20 @@ classB = [(random.normalvariate(0.0, 0.5),
 data = classA + classB
 random.shuffle(data)
 
-kernels = [[linKernel,"Linear"], [polyKernels,"Polynomial"], [RBFKernel,"Radial Basis Function"], [sigmoidKernel,"Sigmoid"]]
+#kernels = [[linKernel,"Linear"], [polyKernels,"Polynomial"], [RBFKernel,"Radial Basis Function"], [sigmoidKernel,"Sigmoid"]]
+
+kernels = [[sigmoidKernel,"Sigmoid"]]
 
 for ker in kernels:
-	f = pylab.figure()
-	f.canvas.set_window_title(ker[1])
-	svmMain(data, ker[0],f.add_subplot(111))
+	for i in pylab.frange(0.1, 0.2, 0.05):
+		for j in pylab.frange(0.0, 0.2, 0.05):
+			f = pylab.figure()
+			f.canvas.set_window_title(ker[1])
+			f.suptitle("Sigmoid k %.3f, delta %.2f" % (i,j))
+			svmMain(data, ker[0],f.add_subplot(111), i, j)
+			f.savefig("SigmoidK%.3fDelta%.2f.png"%(i,j))
 
+print "Done!"
 pylab.show()
 
 
